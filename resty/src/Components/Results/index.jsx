b62133a85d9b792afaf9b4f77c42d299b13be179
abdullah-aljoulani <abdullahaljoulani@gmail.com>
2023-08-29 14:05:00 +0000

    complete the work

--- resty/src/Components/Results/index.jsx
+++ resty/src/Components/Results/index.jsx
@@ -1,6 +1,19 @@
+// import { useState } from "react";
 import JSONPretty from "react-json-prettify";
 
 function Results (props) {
+  // const [method, setMethodd] = useState("GET");
+  // const [url, setUrll] = useState("");
+
+  // const handleSubmit = (e) => {
+  //   console.log(e);
+  //   const formData = {
+  //     method: method,
+  //     url: url,
+  //   };
+  //   props.handleApiCall(formData);
+  //   setMethodd("GET");
+  // };
 
     return (
       <section>
@@ -17,7 +30,29 @@ function Results (props) {
           {props.data.results !== null && (
             <div>
               <h3>Results:</h3>
-              <JSONPretty json={props.data.results} />
+              <JSONPretty
+                json={props.data.results}
+
+              />
+
+              {props.data.results.previous !== null && (
+                <button
+                  onClick={() => {
+                    props.updateUrl(props.data.results.previous);
+                  }}
+                >
+                  Previous
+                </button>
+              )}
+              {props.data.results.next !== null && (
+                <button
+                  onClick={() => {
+                    props.updateUrl(props.data.results.next);
+                  }}
+                >
+                  Next
+                </button>
+              )}
             </div>
           )}
         </pre>
